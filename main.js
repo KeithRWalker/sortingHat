521f3a5b77807791e27d12e6d2f591deb73fcd07
Keith Walker <Keithrwalker418@gmail.com>
2019-03-20 22:56:25 -0500

    wip

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('main.js linked');
+const eventListeners = () => {};
+
+const printToDom = (divId, text) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = text;
+};
+
+
+
+// hogwartsHouses = ['a', 'b', 'c', 'd'];
+// housedStudents= [];
+
+
+// const newStudentBuilder = () => {
+//     // const studentName = document.getElementById("studentNameInput").value;
+//     const studentName = 'name';
+//     const assignedHouse = hogwartsHouses[Math.floor(Math.random()*hogwartsHouses.length)];
+//     const studentObject = {name: '', house: 0};
+//     studentObject.name = studentName;
+//     studentObject.house = assignedHouse;
+//     housedStudents.push(studentObject);
+//     printtodom(div, student object)
+// };
+
+// newStudentBuilder();
