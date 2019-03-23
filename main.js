5b52b482c4118d78778fd47827a6fb24aa67148a
Keith Walker <Keithrwalker418@gmail.com>
2019-03-23 12:54:34 -0500

    form added

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
-const eventListeners = () => {};
+const init = () =>{
+    eventListeners();
+}
+
+const eventListeners = () => {
+    const startBtn = document.getElementById('startBtn');
+    const formDiv = document.getElementById('formDiv');
+    startBtn.addEventListener('click', ()=>{
+        formDiv.style.visibility= 'visible';
+    });
+};
 
 const printToDom = (divId, text) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = text;
 };
 
-
-
-// hogwartsHouses = ['a', 'b', 'c', 'd'];
-// housedStudents= [];
-
-
-// const newStudentBuilder = () => {
-//     // const studentName = document.getElementById("studentNameInput").value;
-//     const studentName = 'name';
-//     const assignedHouse = hogwartsHouses[Math.floor(Math.random()*hogwartsHouses.length)];
-//     const studentObject = {name: '', house: 0};
-//     studentObject.name = studentName;
-//     studentObject.house = assignedHouse;
-//     housedStudents.push(studentObject);
-//     printtodom(div, student object)
-// };
-
-// newStudentBuilder();
+init();
