3a1fddb4acbfdc575f60e43542aee42c10fcae24
Keith Walker <Keithrwalker418@gmail.com>
2019-03-27 03:24:56 -0500

    rewrote the whole damn thing to allow deletes

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const houses = [
     {
         name: 'Gryffindor',
         img: './imgs/g.png',
-        houseNum: 0,
         members: 'Harry Potter, Hermione Granger and Ron Weasley.',
         q: `‘You might belong in Gryffindor,<br>
          Where dwell the brave at heart,<br>
@@ -13,7 +12,6 @@ const houses = [
     {
         name: 'Hufflepuff',
         img: './imgs/h.png',
-        houseNum: 1,
         members: 'Newt Scamander, Cedric Diggory and Nymphadora Tonks.',
         q: `‘You might belong in Hufflepuff<br>
         Where they are just and loyal<br>
@@ -24,7 +22,6 @@ const houses = [
     {
         name: 'Ravenclaw',
         img: './imgs/r.png',
-        houseNum: 2,
         members: 'Luna Lovegood, Gilderoy Lockhart and Filius Flitwick.',
         q: `‘Or yet in wise old Ravenclaw<br>
         If you’ve a ready mind<br>
@@ -35,7 +32,6 @@ const houses = [
     {
         name: 'Slytherin',
         img: './imgs/s.png',
-        houseNum: 3,
         members: 'Severus Snape, Draco Malfoy and Lord Voldemort.',
         q: `‘Or perhaps in Slytherin<br>
         You’ll make your real friends<br>
@@ -44,43 +40,85 @@ const houses = [
         moreInfo: 'https://www.pottermore.com/collection/all-about-slytherin'
     }
 ];
+const studentArray = [];
+let studentCount = 1;
+const inputName = document.getElementById('nameForm');
 
 const init = () =>{
     eventListeners();
-}
+};
+
+const printToDom = (divId, text) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = text;
+};
 
 const eventListeners = () => {
     const startBtn = document.getElementById('startBtn');
     const formDiv = document.getElementById('formDiv');
     const sortBtn = document.getElementById('sortBtn');
     startBtn.addEventListener('click', ()=>{
-        formDiv.style.visibility= 'visible';
+        formDiv.style.visibility = 'visible';
     });
     sortBtn.addEventListener('click', sortingHat);
 };
 
-const printToDom = (divId, text) => {
-    const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML += text;
-};
-
-
-const sortingHat = () => {
-    const studentName = document.getElementById('name');
-    const studentHouse = houses[Math.floor(Math.random() * houses.length)];
-    const cardString =` <div class="card ${studentName.value}Card" id="test" style="width: 18rem;">
-                            <h2>${studentName.value}</h2>
-                            <h4>${studentHouse.name}</h4>
-                            <img src="${studentHouse.img}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h6>Notable Members:</h6>
-                                <p class="card-text">${studentHouse.members}</p>
-                                <a href="${studentHouse.moreInfo}">Learn more about ${studentHouse.name}</a>
-                                <p class="card-text q-text">${studentHouse.q}</p>
-                                <a class="sortBtn btn btn-primary btn-sm" id="sortBtn" role="button">Expel  </a>
-                            </div>
-                        </div>`;
+const cardBuilder = (arrayToPrint) => {
+    let cardString = '';
+    arrayToPrint.forEach((student) =>{
+        cardString += `<div class="card" id="studentCard">
+                <h2>${student.name}</h2>
+                <h4>${student.hName}</h4>
+                <img src="${student.hImg}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <a href="${student.hInfo}">Learn more about ${student.hName}</a>
+                    <br><br>
+                    <h6>Notable Members:</h6>
+                    <p class="card-text">${student.hMembers}</p>
+                    <p class="card-text q-text">${student.hQ}</p>
+                    <a class="expelBtn btn btn-primary btn-sm" id="${student.id}" role="button">Expel</a>
+                </div>
+            </div>`;
+    });
     printToDom('cardCon', cardString);
+    addExpelEvents();
+    inputName.value=null;
 };
 
+const sortingHat = (e) => {
+    e.preventDefault();
+    const studentHouse = houses[Math.floor(Math.random() * houses.length)];
+    const studentName = inputName.value;
+    const newStudent = {
+        name: studentName,
+        id: `student${studentCount}`,
+        hName: studentHouse.name,
+        hImg: studentHouse.img,
+        hMembers: studentHouse.members,
+        hQ: studentHouse.q,
+        hInfo: studentHouse.moreInfo
+    };
+    studentArray.unshift(newStudent);
+    studentCount++;
+    cardBuilder(studentArray);
+};
+
+
+const expelStudent = (e) => {
+    const buttonId = e.target.id;
+    studentArray.forEach((student, index) => {
+        if(student.id === buttonId){
+            studentArray.splice(index, 1);
+        };
+    });
+    cardBuilder(studentArray);
+    addExpelEvents();
+};
+
+const addExpelEvents = () =>{
+    const expelBtn = document.getElementsByClassName('expelBtn');
+    for(i=0; i<expelBtn.length; i++){
+        expelBtn[i].addEventListener('click', expelStudent);
+    };
+};
 init();
